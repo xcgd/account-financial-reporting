22de9608d9dbc2873ff9cd77dfa969a164e4d21a
Vincent Hatakeyama <vincent.hatakeyama@xcg-consulting.fr>
2022-09-07 11:22:15 +0200

    🚑 fix relation level filtering
    There was several issue:
    - the option to hide parent hierarchy was not working
    - the filtering level used was computed differently that the one in the
      computation hierarchy, starting at 0. On top of that children had the
      same level as their parent group, instead of having a different value,
      making it impossible to filter them.
    - the ordering was using a complete code but it did not contain the code of
      the account itself, breaking ordering.
    
    The filtering of accounts and groups is done when generating data, so
    that allows testing and makes sure all reports type display the same
    data.
    
    I18N errors reported by pylint have also been fixed, and black run on
    the module.

--- account_financial_report/report/trial_balance_xlsx.py
+++ account_financial_report/report/trial_balance_xlsx.py
@@ -129,7 +129,8 @@ class TrialBalanceXslx(models.AbstractModel):
         return [
             [
                 _("Date range filter"),
-                _("From: %s To: %s") % (report.date_from, report.date_to),
+                _("From: %(date_from)s To: %(date_to)s")
+                % {"date_from": report.date_from, "date_to": report.date_to},
             ],
             [
                 _("Target moves filter"),
@@ -147,7 +148,7 @@ class TrialBalanceXslx(models.AbstractModel):
             ],
             [
                 _("Limit hierarchy levels"),
-                _("Level %s" % report.show_hierarchy_level)
+                _("Level %s") % report.show_hierarchy_level
                 if report.limit_hierarchy_level
                 else _("No limit"),
             ],
@@ -181,12 +182,8 @@ class TrialBalanceXslx(models.AbstractModel):
         if not show_partner_details:
             for balance in trial_balance:
                 if hierarchy_on == "relation":
-                    if limit_hierarchy_level:
-                        if show_hierarchy_level > balance["level"]:
-                            # Display account lines
-                            self.write_line_from_dict(balance)
-                    else:
-                        self.write_line_from_dict(balance)
+                    # filtering by level is done at the data level
+                    self.write_line_from_dict(balance)
                 elif hierarchy_on == "computed":
                     if limit_hierarchy_level:
                         if show_hierarchy_level == balance["level"] or (
@@ -262,7 +259,7 @@ class TrialBalanceXslx(models.AbstractModel):
             line_object.currency_id = line_object.report_account_id.currency_id
         elif type_object == "account":
             line_object.currency_id = line_object.currency_id
-        super(TrialBalanceXslx, self).write_line(line_object)
+        super().write_line(line_object)
 
     def write_account_footer(self, account, name_value):
         """Specific function to write account footer for Trial Balance"""
